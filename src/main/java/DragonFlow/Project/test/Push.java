056c5d5bb785b27c7304731f99ea2e15b4c56fde
jundi <jundijundy255@gmail.com>
2025-03-18 23:23:13 +0700

    add class

---
+++ src/main/java/DragonFlow/Project/test/Push.java
@@ -0,0 +1,8 @@
+package DragonFlow.Project.test;
+
+public class Push {
+    public static void main(String[] args) {
+        String nama = "Nama Saya Jundi";
+        System.out.println(nama);
+    }
+}
