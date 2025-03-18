609393665fa134e9102c876b8494f4061decb3f2
Jundillah123 <168612649+Jundillah123@users.noreply.github.com>
2025-03-19 00:39:29 +0700

    add file

---
+++ Project/src/main/java/DragonFlow/Project/test/Testing.java
@@ -0,0 +1,20 @@
+package DragonFlow.Project.test;
+
+import ch.qos.logback.core.rolling.helper.IntegerTokenConverter;
+
+public class Testing {
+    public static void main(String[] args) {
+        String name = "jundi";
+        String test = new StringBuilder(name).reverse().toString();
+        System.out.println(test);
+//        int nomber = 23400000;
+        String type = "Ini Type Data double Di Convert ke int : ";
+        double co = 23.0000;
+        int testt = (int) co;
+        System.out.println( type + testt);
+        System.out.println( co);
+
+
+
+    }
+}
---
+++ Project/src/test/java/DragonFlow/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package DragonFlow.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
